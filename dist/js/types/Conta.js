80aba80c7e133e443b58b374840ce6d61244087b
Adriano Dias <adriano.dsm1000@gmail.com>
2024-04-13 20:00:08 -0300

    last commit

--- dist/js/types/Conta.js
+++ dist/js/types/Conta.js
@@ -1,38 +1,30 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { Armazenador } from "./Armazenador.js";
+import { ValidaDebito, ValidaDeposito } from "./Decorators.js";
 import { TipoTransacao } from "./TipoTransacao.js";
-let saldo = JSON.parse(localStorage.getItem('saldo')) || 0;
-const transacoes = JSON.parse(localStorage.getItem('transacoes'), (key, value) => {
-    if (key === 'data') {
-        return new Date(value);
+export class Conta {
+    nome;
+    saldo = Armazenador.obter('saldo') || 0;
+    transacoes = Armazenador.obter(('transacoes'), (key, value) => {
+        if (key === 'data') {
+            return new Date(value);
+        }
+        return value;
+    }) || [];
+    constructor(nome) {
+        this.nome = nome;
     }
-    return value;
-}) || [];
-const depositar = (valor) => {
-    if (valor <= 0) {
-        throw new Error('O valor debitado deve ser maior que zero!');
+    getTitular() {
+        return this.nome;
     }
-    saldo += valor;
-    localStorage.setItem('saldo', saldo.toString());
-};
-const debitar = (valor) => {
-    if (valor <= 0) {
-        throw new Error('O valor depositado deve ser maior que zero!');
-    }
-    if (valor > saldo) {
-        throw new Error('Saldo insuficiente!');
-    }
-    saldo -= valor;
-    localStorage.setItem('saldo', saldo.toString());
-};
-const Conta = {
-    getSaldo() {
-        return saldo;
-    },
-    getDataAcesso() {
-        return new Date();
-    },
     getGruposTransacoes() {
         const gruposTransacoes = [];
-        const listaTransacoes = structuredClone(transacoes);
+        const listaTransacoes = structuredClone(this.transacoes);
         const transacoesOrdenadas = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
         let labelAtualGrupoTransacao = '';
         for (let transacao of transacoesOrdenadas) {
@@ -47,29 +39,43 @@ const Conta = {
             gruposTransacoes.at(-1).transacoes.push(transacao);
         }
         return gruposTransacoes;
-    },
+    }
+    getSaldo() {
+        return this.saldo;
+    }
+    getDataAcesso() {
+        return new Date();
+    }
+    depositar(valor) {
+        this.saldo += valor;
+        Armazenador.salvar('saldo', this.saldo.toString());
+    }
+    debitar(valor) {
+        this.saldo -= valor;
+        Armazenador.salvar('saldo', this.saldo.toString());
+    }
     registrarTransacao(novaTransacao) {
         if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            depositar(novaTransacao.valor);
+            this.depositar(novaTransacao.valor);
         }
         else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-            debitar(novaTransacao.valor);
+            this.debitar(novaTransacao.valor);
             novaTransacao.valor *= -1;
         }
         else {
             throw new Error('Tipo de Transação Inválida!');
         }
-        transacoes.push(novaTransacao);
+        this.transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
-        localStorage.setItem('transacoes', JSON.stringify(transacoes));
-    },
+        Armazenador.salvar('transacoes', JSON.stringify(this.transacoes));
+    }
     agruparTransacoes() {
         const resumo = {
             totalDepositos: 0,
             totalTransferencias: 0,
             totalPagamentosBoleto: 0
         };
-        transacoes.forEach((transacao) => {
+        this.transacoes.forEach((transacao) => {
             switch (transacao.tipoTransacao) {
                 case TipoTransacao.DEPOSITO:
                     resumo.totalDepositos += transacao.valor;
@@ -84,5 +90,22 @@ const Conta = {
         });
         return resumo;
     }
-};
-export default Conta;
+}
+__decorate([
+    ValidaDeposito
+], Conta.prototype, "depositar", null);
+__decorate([
+    ValidaDebito
+], Conta.prototype, "debitar", null);
+export class ContaPremium extends Conta {
+    registrarTransacao(transacao) {
+        if (transacao.tipoTransacao === TipoTransacao.DEPOSITO) {
+            console.log('ganhou um bônus de 0.50 centavos');
+            transacao.valor += 0.5;
+        }
+        super.registrarTransacao(transacao);
+    }
+}
+const conta = new Conta('Joana da Silva Oliveira');
+const contaPremium = new ContaPremium('Adriano Dias');
+export default conta;
